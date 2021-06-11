4aee0877ba259e554c467ace24a4195dd09d9fea
Martin Matusiak <numerodix@gmail.com>
2021-06-11 15:06:06 +1000

    listing different kinds

--- try.py
+++ try.py
@@ -4,37 +4,54 @@ import asyncio
 
 class KubeCli:
     def __init__(self) -> None:
-        pass
+        self.session = aiohttp.ClientSession()
 
-    async def list_objects(self, kind='namespaces'):
-        url = 'http://127.0.0.1:8001/api/v1/%s' % kind
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                js = await response.json()
-                # import ipdb as pdb; pdb.set_trace() # BREAKPOINT
+    async def list_objects(self, prefix='/api/v1', kind='namespaces'):
+        url = 'http://127.0.0.1:8001/%s/%s' % (prefix, kind)
+        async with self.session.get(url) as response:
+            js = await response.json()
+            try:
                 items = js['items']
+            except KeyError:
+                import ipdb as pdb; pdb.set_trace() # BREAKPOINT
 
-                for item in items:
-                    item['apiVersion'] = js['apiVersion']
-                    item['kind'] = js['kind'].replace('List', '')
+            for item in items:
+                item['apiVersion'] = js['apiVersion']
+                item['kind'] = js['kind'].replace('List', '')
 
-                return items
+            return items
 
     async def list_all(self):
-        nss = self.list_objects(kind='namespaces')
-        pods = self.list_objects(kind='pods')
-        lists = [pods, nss]
+        lists = [
+            self.list_objects(kind='configmaps'),
+            self.list_objects(kind='namespaces'),
+            self.list_objects(kind='nodes'),
+            self.list_objects(kind='pods'),
+            self.list_objects(kind='secrets'),
+            self.list_objects(prefix='/apis/argoproj.io/v1alpha1', kind='applications'),
+            self.list_objects(prefix='/apis/argoproj.io/v1alpha1', kind='rollouts'),
+            self.list_objects(prefix='/apis/networking.istio.io/v1beta1', kind='destinationrules'),
+            self.list_objects(prefix='/apis/networking.istio.io/v1beta1', kind='gateways'),
+            self.list_objects(prefix='/apis/networking.istio.io/v1beta1', kind='serviceentries'),
+            self.list_objects(prefix='/apis/networking.istio.io/v1beta1', kind='sidecars'),
+            self.list_objects(prefix='/apis/networking.istio.io/v1beta1', kind='virtualservices'),
+        ]
         lists = await asyncio.gather(*lists)
         for lst in lists:
             for obj in lst:
                 yield obj
 
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, *args, **kwargs):
+        await self.session.__aexit__(*args, **kwargs)
+
 
 async def main():
-    cli = KubeCli()
-    # objs = await cli.list_all()
-    async for obj in cli.list_all():
-        print(obj['kind'], obj['metadata']['name'])
+    async with KubeCli() as cli:
+        async for obj in cli.list_all():
+            print(obj['kind'], obj['metadata']['name'])
 
 
 loop = asyncio.get_event_loop()
